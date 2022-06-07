b1488200c725de6f6a4014842fa659d2b14736ea
lainevesku <lainevesku@gmail.com>
2022-06-07 14:19:26 +0300

    header komponennti ja tyylimääreet

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,11 @@
 import styles from './app.module.scss';
+import Header from '../header/header';
 import Items from '../items/items';
 
 function App() {
   return (
     <div className={styles.app}>
+        <Header />
         <Items />
     </div>
   );
---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import styles from './header.module.scss';
+
+function Header() {
+    return(
+        <div className={styles.header}>
+            Project Kymppi
+        </div>
+    );
+}
+
+export default Header;
