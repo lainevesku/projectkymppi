54f72d7bbcf6eb98aba26398fb530c1d984fad1d
lainevesku <lainevesku@gmail.com>
2022-07-20 02:46:27 +0300

    laskimen ulkonäkö ja vastausten pyöristys

--- src/routes/calculator/calculator.js
+++ src/routes/calculator/calculator.js
@@ -13,9 +13,9 @@ function Calculator() {
         let kattomalli = e.value;
         let LapePA ="Lappeen pinta-ala: " + korkeus * leveys + "m²";
         let otsalautamaara = "Otsalauta(23*170): " + ((korkeus * 2) + leveys) * otsalauta + " metriä";
-        let pystyruode = "Pystyruode(22*100): " + leveys / kattotuoli * korkeus * 1.05 + " metriä"; // pyörustä yksi ylöspäin esim 58.3333 on 59
-        let tuuletusrima = "Tuuletusrima(22*50): " + leveys / kattotuoli * korkeus * 1.05 + " metriä";
-        let vaakaruode = "Vaakaruode(32*100): " + korkeus / kattomalli * leveys * 1.05 + " metriä";
+        let pystyruode = "Pystyruode(22*100): " + Math.ceil(leveys / kattotuoli * korkeus * 1.05) + " metriä"; 
+        let tuuletusrima = "Tuuletusrima(22*50): " + Math.ceil(leveys / kattotuoli * korkeus * 1.05) + " metriä";
+        let vaakaruode = "Vaakaruode(32*100): " + Math.ceil(korkeus / kattomalli * leveys * 1.05) + " metriä";
         
         return (
         document.getElementById("tulos").innerHTML = LapePA + "<br />" + otsalautamaara + "<br />" + pystyruode + "<br />" + tuuletusrima + "<br />" + vaakaruode
@@ -23,21 +23,14 @@ function Calculator() {
     }
 
     return(
-        <div>
+
+        <div className={styles.calculator}>
 
         <div>
             <h2>Lape laskuri</h2>
         </div>
 
-        <div className={styles.calculator}>
-            <div>
-            <label htmlFor='kattomalli'>Katon tyyppi</label>
-                <select name='kattomalli' id='kattomalli'>
-                    <option value="0.35">Tiilikuvio</option>
-                    <option value="0.30">Lukkosauma</option>
-                    <option value="0.35">Tiilikatto</option>
-                </select>
-            </div>
+        <div className={styles.calculator_row}>
             <div>
                 <label htmlFor="korkeus">Lappeen korkeus (m)</label>
                 <input type='number' id='korkeus' name='korkeus' min="0" step="0.1" />
@@ -48,7 +41,7 @@ function Calculator() {
             </div>
         </div>
 
-        <div className={styles.calculator}>
+        <div className={styles.calculator_row}>
             <div>
                 <label for="kattotuoli">Kattotuolijako</label>
                 <input type='number' id='kattotuoli' name='kattotuoli' min="0" step="0.1" />
@@ -59,9 +52,22 @@ function Calculator() {
             </div>
         </div>
 
-        <div className={styles.calculator}>
+        <div className={styles.calculator_row}>
+            <div>
+                <label htmlFor='kattomalli'>Katon tyyppi</label>
+                <select name='kattomalli' id='kattomalli'>
+                    <option value="0.35">Tiilikuvio</option>
+                    <option value="0.30">Lukkosauma</option>
+                    <option value="0.35">Tiilikatto</option>
+                </select>
+            </div>
             <div>
                 <Button secondary onClick={laske}>LASKE</Button>
+            </div>
+        </div>
+
+        <div className={styles.calculator}>
+            <div>
                 <div id="tulos"></div>
             </div>
         </div>
