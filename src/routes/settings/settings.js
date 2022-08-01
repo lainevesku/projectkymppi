f9bdfda3f7248f90b502ccdf4447de86d6fdbdfd
lainevesku <lainevesku@gmail.com>
2022-08-01 22:58:27 +0300

    settings sivulle lisätty ohjeita

--- src/routes/settings/settings.js
+++ src/routes/settings/settings.js
@@ -24,9 +24,58 @@ function Settings(props) {
                     <div><img src={user.data.photoURL} alt="" /></div>
                     <div>{user.data.displayName}<br/>{user.data.email}</div>
                 </div>
-                <div>
+                <div className={styles.settings_button}>
                     <Button secondary onClick={signOut}>Kirjaudu ulos</Button>
                 </div>
+                <h2>Ohjeita La'Peten käyttöön</h2>
+                <div className={styles.settings_images}>
+                    <img src={require("../../assets/images/Etusivu.png")} alt='' />
+                    <p>Etusivulta löydät kaikki lisäämäsi työmaat aikajärjestyksessä
+                       uusimmasta vanhimpaan. Etusivun oikeassa alakulmassa on nappi,
+                       jossa on sinisellä taustalla katon kuva. Sitä painamalla pääset
+                       lisäämään työmaita listaasi. Laittamalla hiiren työmaan nimen
+                       päälle, ruutuun ilmestyy musta nuoli harmaalla taustalla, jota painamalla pääset
+                       näkemään tarkemmat tiedot työmaastasi. Alareunassa on vielä 
+                       kolme kuvaketta. Vasemmalta ensimmäistä painamalla palaat etusivulle.
+                       Keskimmäistä painamalla pääset laskin sivulle ja viimeisestä
+                       pääset tälle sivulle, mistä löytyy ohjeet ja uloskirjautuminen.
+                    </p>
+                </div>
+                <div className={styles.settings_images}>
+                    <img src={require("../../assets/images/Lisaasivu.png")} alt='' />
+                    <p>Työmaan lisäämissivulla voit antaa työmaan tiedot lomakkeelle.
+                       Lomakkeelle voit lisätä työmaan perustietoja, kuten asiakkaan
+                       nimi ja osoite. Lomakkeella voit lisätä myös työmaasta saamasi
+                       palkan ja merkitä koska työt alkoivat ja koska työmaa saatiin
+                       valmiiksi. Sen jälkeen kun nämä tiedot on lisätty voit valita,
+                       minkä katon asiakas on valinnut kuin myös katon värin. Lomakkeelta
+                       löytyy 6 lape-nappia ja aina yhtä nappia painamalla saa auki lomakkeen
+                       mihin yhden lappeen tiedot laitetaan. Tämän jälkeen voit halutessasi sulkea kyseisen 
+                       lappeen tuplapainamalla lape-nappia. Toista tämä niin monta kertaa kuin lappeita
+                       on urakassasi. Lopuksi viimeisenä on tekstikenttä, johon voit laittaa muita tietoja,
+                       jos on jotain mitä haluat itsellesi erikseen muistiin.
+                    </p>
+                </div>
+                <div className={styles.settings_images}>
+                    <img src={require("../../assets/images/Tiedot.png")} alt='' />
+                    <p>Työmaa sivulta löydät kaikki äsken lisäämäsi tiedot. La'Pete myös
+                       laskee antamisesi päivien perusteella kuinka monta arkipäivää
+                       työmaalla ollaan oltu ja laskee kanssa keskimääräisen päiväpalkan
+                       urakalle. La'Pete laskee antamiesi tietojen perusteella kuinka paljon
+                       työmaalla tarvitaan eri puutavaraa ja paljonko tarvitaan peltejä/tiiliä.
+                       Ylimmäisenä on kaikki tavarat yhteenlaskettuna ja sen jälkeen löydät jokaisen 
+                       lappeen tavarat erikseen eriteltyinä.
+                    </p>
+                </div>
+                <div className={styles.settings_images}>
+                    <img src={require("../../assets/images/Laskin.png")} alt='' />
+                    <p>Lape laskuri sivulta löydät samanlaisen lomakkeen, mitä työmaan lisäämissivulla
+                       lape-nappia painamalla ilmestyy. Syöttämällä lappeen korkeuden, leveyden,
+                       kattotuolijaon ja otsalautakierroksen ja valitsemalla katon tyypin La'Pete
+                       laskee niiden perusteella kuinka paljon mitäkin tavaraa yksi lape tarvitsee.
+                    </p>
+                </div>
+
             </div>  
 
         </div>
